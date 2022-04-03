58f1922ecb27d117e8e1e90703295eeadc20768f
jolson2811 <jolson2811@gmail.com>
2022-04-03 16:04:50 -0600

    Prompt user for data and capture user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,124 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const {generateMarkdown, writeFile} = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+let userInput = {
+    title: "",
+    description: "",
+    instruction: "",
+    usage: "",
+    guideline: "",
+    testInstruction: "",
+    gitHub: ""
+}
+
+// TODO: create input function
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project title?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub link',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub link!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter in your project description',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project description.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter in your projects installation instructions',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your instructions');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter your project usage information',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your usage information');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'guidelines',
+            message: 'Enter your contribution guidelines',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your contribution guidelines');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'testInstructions',
+            message: 'Enter your project test instructions',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your test instructions');
+                    return false;
+                }
+            }
+        },
+    ]);
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser().then(function(res){
+        console.log("response from questions",res);
+        generateMarkdown(res);
+    });
+}
 
 // Function call to initialize app
 init();
